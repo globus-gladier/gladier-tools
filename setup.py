e5da8020bf6b42973afc21194bce321563d22550
Rafael Vescovi <ravescovi@gmail.com>
2021-03-17 16:21:43 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gladier_tools',
+      version='0.1-dev-raf',
+      description='Basic Tooling for APS beamlines',
+      url='https://github.com/ANL-Braid/Flows_examples.git',
+      author='Rafael Vescovi',
+      author_email='ravescovi@gmail.com',
+      license='MIT',
+      packages=['gladier_tools'],
+      zip_safe=False)
