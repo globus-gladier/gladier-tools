8ee3010d682e3cd809367d07cac918f8b7661aa0
Nickolaus D. Saint <nick@globus.org>
2021-04-02 07:28:25 -0700

    Wrap plotting in an exception to catch it.
    For some reason, I think this may have been causing funcx tasks to
    die instead of reporting an exception. This may be due to something
    raising a sys.exit() exception somewhere...

--- gladier_tools/xpcs/plot.py
+++ gladier_tools/xpcs/plot.py
@@ -7,5 +7,8 @@ def make_corr_plots(event):
     import os
     from XPCS.tools import xpcs_plots
     os.chdir(os.path.join(event['proc_dir'], os.path.dirname(event['hdf_file'])))
-    xpcs_plots.make_plots(os.path.join(event['proc_dir'], event['hdf_file']))
+    try:
+        xpcs_plots.make_plots(os.path.join(event['proc_dir'], event['hdf_file']))
+    except (Exception, SystemExit) as e:
+        return str(e)
     return [img for img in os.listdir('.') if img.endswith('.png')]
