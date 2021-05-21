922cfa3aaa3f9e2c2295e73334b566adecdb04b0
Rafael Vescovi <ravescovi@gmail.com>
2021-05-21 16:00:45 -0500

    refactor

--- gladier_tools/__init__.py
+++ gladier_tools/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from .globus.globus_transfer import Transfer
+from .posix.posix_tar import Tar
+from .posix.https_download_file import HttpsDownloadFile
+from .posix.posix_untar import UnTar
+
+__all__ = ['HttpsDownloadFile', 'UnTar', 'Tar', 'Transfer']
--- gladier_tools/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .globus_transfer import Transfer
-from .posix_tar import Tar
-from .https_download_file import HttpsDownloadFile
-from .posix_untar import UnTar
-
-__all__ = ['HttpsDownloadFile', 'UnTar', 'Tar', 'Transfer']
-
