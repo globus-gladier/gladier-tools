b731481350d595ab7c3cddbc97639d88e0f7725f
Rohith <rohithj@uchicago.edu>
2021-07-20 13:59:14 -0500

    Reformatted code

--- gladier_tools/posix/encrypt.py
+++ gladier_tools/posix/encrypt.py
@@ -10,33 +10,35 @@ def encrypt(**data):
     import base64
 
     password = bytes(data['encrypt_key'], 'utf-8')
-    salt= b'\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00'
+    salt = b'\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00'
     # salt = os.urandom(16)
 
-    #PBKDF2HMAC is a key derivation function, which is used to derive a 
-    #URL-safe base64-encoded 32-byte key from the initial user input password
-    #Cryptography library documentation: https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
-    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=100000)
+    # PBKDF2HMAC is a key derivation function, which is used to derive a
+    # URL-safe base64-encoded 32-byte key from the initial user input password
+    # Cryptography library documentation: https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32,
+                     salt=salt, iterations=100000)
     key = base64.urlsafe_b64encode(kdf.derive(password))
     fernet = Fernet(key)
-    
-    infile= data['encrypt_input']
+
+    infile = data['encrypt_input']
     if '~' in infile:
         infile = os.path.expanduser(infile)
-    
-    outfile= infile+".aes"
-    
+
+    outfile = infile+".aes"
+
     if os.path.isdir(infile):
-        raise Exception("Please input the path to a file or a tarred directory.")
+        raise Exception(
+            "Please input the path to a file or a tarred directory.")
 
     # opening the original file to encrypt
     with open(infile, 'rb') as file:
         original = file.read()
-        
+
     # encrypting the file
     encrypted = fernet.encrypt(original)
-    
-    # opening the file in write mode and 
+
+    # opening the file in write mode and
     # writing the encrypted data
     with open(outfile, 'wb+') as encrypted_file:
         encrypted_file.write(encrypted)
@@ -49,6 +51,6 @@ class Encrypt(GladierBaseTool):
     funcx_functions = [encrypt]
     required_input = [
         'encrypt_input',
-        'encrypt_key', 
+        'encrypt_key',
         'funcx_endpoint_compute'
-        ]
+    ]
