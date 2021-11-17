3d6e177c7bef920d52061ffa36970a2cdaf5ab55
Nickolaus D. Saint <nick@globus.org>
2021-11-17 14:18:20 -0800

    fix: Untar using old version of funcx action provider (#41)
    Fixed using generate_flow_definition

--- gladier_tools/posix/untar.py
+++ gladier_tools/posix/untar.py
@@ -1,4 +1,4 @@
-from gladier import GladierBaseTool
+from gladier import GladierBaseTool, generate_flow_definition
 
 
 def untar_file(**data):
@@ -6,7 +6,7 @@ def untar_file(**data):
     import tarfile
 
     ##minimal data inputs payload
-    file_path = data.get('file_path', '')
+    file_path = os.path.expanduser(data.get('file_path', ''))
     file_name = data.get('file_name', '')
     output_path = data.get('output_path', '')
     ##
@@ -23,6 +23,11 @@ def untar_file(**data):
         file.extractall(output_path)
     return output_path
 
+
+@generate_flow_definition(modifiers={
+    'untar_file': {'ExceptionOnActionFailure': True,
+                   'WaitTime': 300}
+})
 class UnTar(GladierBaseTool):
     """
     The UnTar tool makes it possible to extract data from Tar archives.
@@ -33,31 +38,6 @@ class UnTar(GladierBaseTool):
     :param funcx_endpoint_non_compute: By default, uses the ``non-compute`` funcx endpoint.
     :returns output_path: Location of the extracted files.
     """
-
-    flow_definition = {
-        'Comment': 'UnTar a single file',
-        'StartAt': 'UnTar',
-        'States': {
-            'UnTar': {
-                'ActionUrl': 'https://api.funcx.org/automate',
-                'Comment': None,
-                'ExceptionOnActionFailure': True,
-                'Parameters': {
-                    'tasks': [
-                        {
-                            'endpoint.$': '$.input.funcx_endpoint_non_compute',
-                            'func.$': '$.input.untar_file_funcx_id',
-                            'payload.$': '$.input'
-                        }
-                    ]
-                },
-                'ResultPath': '$.UnTar',
-                'Type': 'Action',
-                'WaitTime': 300,
-                'End': True,
-            },
-        }
-    }
 
     funcx_functions = [untar_file]
 
