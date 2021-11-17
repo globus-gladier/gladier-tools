b4578849d0fea04bea0426e17afb784e8bd419cf
Nickolaus D. Saint <nick@globus.org>
2021-11-17 14:49:13 -0800

    refactor: Changed untar arguments to match tar

--- gladier_tools/posix/untar.py
+++ gladier_tools/posix/untar.py
@@ -1,50 +1,42 @@
 from gladier import GladierBaseTool, generate_flow_definition
 
 
-def untar_file(**data):
-    import os
+def untar(**data):
     import tarfile
+    import pathlib
 
-    ##minimal data inputs payload
-    file_path = os.path.expanduser(data.get('file_path', ''))
-    file_name = data.get('file_name', '')
-    output_path = data.get('output_path', '')
-    ##
+    untar_input = pathlib.Path(data['untar_input']).expanduser()
+    untar_output = None
+    if data.get('untar_output', ''):
+        untar_output = pathlib.Path(data['untar_output'])
 
-    full_path = os.path.join(file_path, file_name)
-
-    if not os.path.isfile(full_path):
-        raise NameError(f'{full_path}  does not exist!!')
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    
-    with tarfile.open(full_path) as file:
-        file.extractall(output_path)
-    return output_path
+    with tarfile.open(untar_input) as file:
+        if untar_output:
+            untar_output.mkdir(parents=True)
+            file.extractall(untar_output)
+        else:
+            file.extractall()
+    return str(untar_output)
 
 
 @generate_flow_definition(modifiers={
-    'untar_file': {'ExceptionOnActionFailure': True,
-                   'WaitTime': 300}
+    'untar': {'ExceptionOnActionFailure': True,
+              'WaitTime': 300}
 })
 class UnTar(GladierBaseTool):
     """
     The UnTar tool makes it possible to extract data from Tar archives.
 
-    :param file_path: Directory in which the tar file exists. Defaults to empty string if not passed.
-    :param file_name: Name of the .tgz file which has to be untarred. Defaults to empty string if not passed.
-    :param output_path: Location where the files will be extracted from the archive. Defaults to empty string if not passed.
-    :param funcx_endpoint_non_compute: By default, uses the ``non-compute`` funcx endpoint.
-    :returns output_path: Location of the extracted files.
+    :param untar_input: Input directory to archive.
+    :param untar_output: (optional) output file to save the new archive. Defaults to the original
+                       input file with an extension '.tgz' removed.
+    :param funcx_endpoint_compute: By default, uses the ``compute`` funcx endpoint.
+    :returns path: The name of the newly created archive.
     """
 
-    funcx_functions = [untar_file]
+    funcx_functions = [untar]
 
     required_input = [
-        'file_path',
-        'file_name',
-        'output_path',
-        'funcx_endpoint_non_compute'
+            'untar_input',
+            'funcx_endpoint_compute',
         ]
-
