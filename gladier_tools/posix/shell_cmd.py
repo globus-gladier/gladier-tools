a2d2cdc7779bf43ec82c361064d4a90a1d248c5e
Nickolaus D. Saint <nick@globus.org>
2022-07-21 09:54:31 -0700

    Shell CMD: Update docs, update default exception behavior in docs

--- gladier_tools/posix/shell_cmd.py
+++ gladier_tools/posix/shell_cmd.py
@@ -15,7 +15,16 @@ def shell_cmd(
     **kwargs,
 ):
     """Run a command in a shell with various options. Suitable for use as a
-    funcx function.
+    funcx function. On anything other than exit success (returning 0), an
+    exception will be raised, the stack trace will be propogated, and the flow
+    will be halted. On exit success, the return code, stdout, and sterr are returned
+    in a list in the format:
+
+    [0, "Standard Output Text", "Standard Error Text"]
+
+    Note: stdout and sterr will always be None unless ``capture_output`` is set.
+
+
 
     Args:
         args: Arguments to the command. Can be either a list of strings
@@ -35,7 +44,7 @@ def shell_cmd(
             seconds.
         exception_on_error: If the command fails, should an exception be raised
             or should just the (presumably non-zero) return code be returned.
-            Defaults to False.
+            Defaults to True.
         input_path: A path to a file which should be used as the standard input
             to the command. When not provided, stdin does not exist so attempts
             to read from it will result in an error.
