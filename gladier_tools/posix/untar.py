f5d11f4060f995c745d7628e92b5f67ca6f68ead
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 05:54:22 +0000

    Adding tarfile member sanitization to extractall()

--- gladier_tools/posix/untar.py
+++ gladier_tools/posix/untar.py
@@ -13,7 +13,29 @@ def untar(**data):
 
     with tarfile.open(untar_input) as file:
         untar_output.mkdir(parents=True, exist_ok=True)
-        file.extractall(untar_output)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(file, untar_output)
     return str(untar_output)
 
 
