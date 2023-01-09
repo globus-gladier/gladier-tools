fac900c723ce0673c14498cbd0a20870d9f1a47c
Nickolaus D. Saint <nick@globus.org>
2023-01-09 11:21:41 -0800

    refactor: Refactor patch checking code for Untar tool, add tests (#62)

--- gladier_tools/tests/posix/conftest.py
+++ gladier_tools/tests/posix/conftest.py
@@ -10,4 +10,8 @@ def mock_tar(monkeypatch):
     mock_open.return_value.__enter__ = Mock(return_value=mock_tf)
     mock_open.return_value.__exit__ = Mock(return_value=None)
     monkeypatch.setattr(tarfile, 'open', mock_open)
+
+    tarmembers = [tarfile.TarInfo(t) for t in
+                  ('one.txt', 'two.txt', 'three.txt')]
+    mock_tf.getmembers.return_value = tarmembers
     return mock_open, mock_tf
